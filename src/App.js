0e0e9b97e091af3e68843cd45863d8a8e9de3f65
Joe Martella <martellaj@live.com>
2018-05-28 16:13:21 -0700

    Adds favicon and does general clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import './App.css';
 import data from './data';
 import React, { Component } from 'react';
 import TheRose from './TheRose';
 
 class App extends Component {
     render() {
-        const teams = data.teams;
-
-        return (
-            <div className="App">
-                <TheRose teams={teams} />
-            </div>
-        );
+        return <TheRose teams={data.teams} />;
     }
 }
 
