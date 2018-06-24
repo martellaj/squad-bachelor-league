4f94bd988e47c58002b52e760972ce033501b1f2
Joe Martella <martellaj@live.com>
2018-06-24 10:35:38 -0700

    Updates last updated string

--- src/TheRose.js
+++ src/TheRose.js
@@ -1,139 +1,149 @@
-import "./TheRose.css";
-import React, { Component } from "react";
+import './TheRose.css';
+import React, { Component } from 'react';
 
 export default class TheRose extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      standing: null
-    };
+        this.state = {
+            standing: null
+        };
 
-    this.getStandings = this.getStandings.bind(this);
-  }
-
-  render() {
-    const standings = this.getStandings();
-
-    return (
-      <div className="rose-container">
-        <div className="rose-title-container">
-          <div>
-            <span className="rose-title">The Rose™</span> Standings
-          </div>
-          <span className="rose-last-updated">
-            (up-to-date with episode #2)
-          </span>
-        </div>
-        {this.renderStandings(standings)}
-        {this.renderPlayers()}
-      </div>
-    );
-  }
-
-  getStandings() {
-    const standings = [];
-
-    // Get team's points derived from player points.
-    for (const team of this.props.teams) {
-      let points = 0;
-
-      for (const player of team.players) {
-        points += player.points;
-      }
-
-      standings.push({
-        name: team.name,
-        points: points,
-        players: team.players
-      });
+        this.getStandings = this.getStandings.bind(this);
     }
 
-    // Sort standings bsaed on points, then name.
-    standings.sort((a, b) => {
-      if (a.points > b.points) {
-        return -1;
-      } else if (a.points < b.points) {
-        return 1;
-      } else {
-        return a.name > b.name;
-      }
-    });
+    render() {
+        const standings = this.getStandings();
 
-    return standings;
-  }
-
-  renderStandings(standings) {
-    const standingElements = [];
-
-    for (const standing of standings) {
-      standingElements.push(
-        <div key={standing.name} className="rose-standing-container">
-          <a
-            className="rose-standing-name"
-            href="#"
-            onClick={() => this.onNameClick(standing)}
-          >
-            {standing.name}
-          </a>{" "}
-          <span className="rose-standing-points">{standing.points}</span>
-        </div>
-      );
+        return (
+            <div className="rose-container">
+                <div className="rose-title-container">
+                    <div>
+                        <span className="rose-title">The Rose™</span> Standings
+                    </div>
+                    <span className="rose-last-updated">
+                        (up-to-date with episode #4)
+                    </span>
+                </div>
+                {this.renderStandings(standings)}
+                {this.renderPlayers()}
+            </div>
+        );
     }
 
-    return standingElements;
-  }
+    getStandings() {
+        const standings = [];
 
-  onNameClick(standing) {
-    if (standing.name === (this.state.standing && this.state.standing.name)) {
-      this.setState({
-        standing: null
-      });
-    } else {
-      this.setState({
-        standing: standing
-      });
-    }
-  }
+        // Get team's points derived from player points.
+        for (const team of this.props.teams) {
+            let points = 0;
 
-  renderPlayers() {
-    const standing = this.state.standing;
+            for (const player of team.players) {
+                points += player.points;
+            }
 
-    if (standing) {
-      return (
-        <div className="players-container">
-          <div>
-            <strong>{standing.name}'s Team</strong>
-          </div>
-          <div>{this.getPlayerElements(standing.players)}</div>
-        </div>
-      );
-    }
-  }
+            standings.push({
+                name: team.name,
+                points: points,
+                players: team.players
+            });
+        }
 
-  getPlayerElements(players) {
-    const playerElements = [];
+        // Sort standings bsaed on points, then name.
+        standings.sort((a, b) => {
+            if (a.points > b.points) {
+                return -1;
+            } else if (a.points < b.points) {
+                return 1;
+            } else {
+                return a.name > b.name;
+            }
+        });
 
-    for (const player of players) {
-      playerElements.push(
-        <div
-          className="player-container"
-          onClick={() => window.open(player.bio, "_blank")}
-        >
-          <span className={player.eliminated && "player-name-eliminated"}>
-            {player.name} (pick #{player.pick})
-          </span>
-          {player.note && <div className="player-note">{player.note}</div>}
-          <img
-            className={`player-picture ${
-              player.eliminated ? "player-picture-eliminated" : ""
-            }`}
-            src={player.picture}
-          />
-        </div>
-      );
+        return standings;
     }
 
-    return playerElements;
-  }
+    renderStandings(standings) {
+        const standingElements = [];
+
+        for (const standing of standings) {
+            standingElements.push(
+                <div key={standing.name} className="rose-standing-container">
+                    <a
+                        className="rose-standing-name"
+                        href="#"
+                        onClick={() => this.onNameClick(standing)}
+                    >
+                        {standing.name}
+                    </a>{' '}
+                    <span className="rose-standing-points">
+                        {standing.points}
+                    </span>
+                </div>
+            );
+        }
+
+        return standingElements;
+    }
+
+    onNameClick(standing) {
+        if (
+            standing.name === (this.state.standing && this.state.standing.name)
+        ) {
+            this.setState({
+                standing: null
+            });
+        } else {
+            this.setState({
+                standing: standing
+            });
+        }
+    }
+
+    renderPlayers() {
+        const standing = this.state.standing;
+
+        if (standing) {
+            return (
+                <div className="players-container">
+                    <div>
+                        <strong>{standing.name}'s Team</strong>
+                    </div>
+                    <div>{this.getPlayerElements(standing.players)}</div>
+                </div>
+            );
+        }
+    }
+
+    getPlayerElements(players) {
+        const playerElements = [];
+
+        for (const player of players) {
+            playerElements.push(
+                <div
+                    className="player-container"
+                    onClick={() => window.open(player.bio, '_blank')}
+                >
+                    <span
+                        className={
+                            player.eliminated && 'player-name-eliminated'
+                        }
+                    >
+                        {player.name} (pick #{player.pick})
+                    </span>
+                    {player.note && (
+                        <div className="player-note">{player.note}</div>
+                    )}
+                    <img
+                        className={`player-picture ${
+                            player.eliminated ? 'player-picture-eliminated' : ''
+                        }`}
+                        src={player.picture}
+                    />
+                </div>
+            );
+        }
+
+        return playerElements;
+    }
 }
